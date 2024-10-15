c2fa2692dd8dad124d9c0178bcf691d124520ad5
Juliana Ifionu <julieifionu@gmail.com>
2024-10-15 19:00:52 -0400

    Add function to create a campaign

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -3,4 +3,52 @@ pragma solidity ^0.8.9;
 
 contract CrowdFunding {
     constructor() {}
+
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        require(_deadline > block.timestamp, "Deadline must be in the future.");
+
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        uint256 campaignIndex = numberOfCampaigns;
+
+        numberOfCampaigns++;
+
+        return campaignIndex;
+    }
+
+    function getCampaigns() external {}
+
+    function donateToCampaign() external {}
+
+    function getDonators() external {}
 }
