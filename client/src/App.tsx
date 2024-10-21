78b2792e07bf4c1da52251d20b94692d9db0b2e5
Juliana Ifionu <julieifionu@gmail.com>
2024-10-21 16:15:18 -0400

    Initialize a vite app with thirdweb sdk

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+      hello
+    </main>
+  );
+}
