49068f6b004757ef899a1587a64442df0632354c
Juliana Ifionu <julieifionu@gmail.com>
2024-10-21 07:34:36 -0400

    Add my configuration

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,44 +1,22 @@
-require("@matterlabs/hardhat-zksync-solc");
-require("dotenv").config();
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
-  networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
-    },
-  },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
-  solidity: {
-    version: "0.8.17",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+	solidity: {
+		version: '0.8.9',
+		defaultNetwork: 'sepolia',
+		networks: {
+			hardhat: {},
+			sepolia: {
+				url: process.env.ALCHEMY_TESTNET_RPC_URL,
+				accounts: [`0x${process.env.TESTNET_PRIVATE_KEY}`],
+			},
+		},
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
 };
