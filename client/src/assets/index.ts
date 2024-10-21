1b006b32a01db5bb45607d5cf43bd142f3224767
Juliana Ifionu <julieifionu@gmail.com>
2024-10-21 17:00:52 -0400

    Add assets

---
+++ client/src/assets/index.ts
@@ -0,0 +1,9 @@
+import sun from "./sun.svg";
+import createCampaign from "./create-campaign.svg";
+import dashboard from "./dashboard.svg";
+import logout from "./logout.svg";
+import payment from "./payment.svg";
+import profile from "./profile.svg";
+import withdraw from "./withdraw.svg";
+
+export { sun, createCampaign, dashboard, logout, payment, profile, withdraw };
