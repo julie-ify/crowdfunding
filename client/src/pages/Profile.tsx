a8aaee54edd68c52acfe428fdc64e83958b1c659
Juliana Ifionu <julieifionu@gmail.com>
2024-10-30 05:53:18 -0400

    Implement profile page to display user's campaigns

---
+++ client/src/pages/Profile.tsx
@@ -0,0 +1,33 @@
+import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components";
+import { ethers } from "ethers";
+
+function Profile() {
+  const { getUserCampaigns, isPending } = useStateContext();
+	const campaignLists = getUserCampaigns();
+
+  const parsedCampaigns = campaignLists
+    ? campaignLists.map((campaign, i) => ({
+        campaignId: i,
+        owner: campaign.owner,
+        title: campaign.title,
+        description: campaign.description,
+        target: ethers.formatUnits(campaign.target),
+        deadline: Number(campaign.deadline),
+        amountCollected: ethers.formatUnits(campaign.amountCollected),
+        image: campaign.image,
+      }))
+    : [];
+
+  return (
+    <>
+      <DisplayCampaigns
+        title="All Campaigns"
+        isPending={isPending}
+        campaigns={parsedCampaigns}
+      />
+    </>
+  );
+}
+
+export default Profile;
